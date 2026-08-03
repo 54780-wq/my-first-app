2ecb5c10977891a738c20d1abfb57a5da9218525
54780-wq <54780@yupparaj.ac.th>
2026-08-03 13:19:13 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -6,4 +6,4 @@ net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นายณฐพัฒน์ ไชยพานิชย์ เลขที่34 ม.4/3")
