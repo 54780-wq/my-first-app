1148833481b7df0bb8502cbd2d541027acb81011
54780-wq <54780@yupparaj.ac.th>
2026-08-10 13:41:46 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -1,24 +1,25 @@
-import streamlit as st
+port streamlit as st
 
 st.markdown("# :red[🏋️ คํานวนค่าดัชนีมวลกาย BMI]")
-st.write("กรอกข้อมูลนํ้าหนักเเละส่วนวูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น")
+st.write("กรอกข้อมูลนํ้าหนักเเละส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น")
 
-weight = st.number_input("กรอกนํ้าหนักของคุณ (กิโลกรัม):",min_value=1.0)
-height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซ็นติเมตร):",min_value=1.0)
+weight = st.number_input("กรอกนํ้าหนักของคุณ (กิโลกรัม):", min_value=1.0)
+height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0)
 
 if st.button("คํานวนค่า BMI 🎯"):
-  height_m = height_cm / 100
-  bmi = weight("---")
-  st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+    height_m = height_cm / 100
+    bmi = weight / (height_m ** 2)
+    
+    st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
 
-if bmi < 18.5:
-    st.warning("⚠️ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
-elif 18.5 <= bmi < 23.0:
-    st.success("🎉 คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
-elif 23.0 <= bmi < 25.0:
-    st.info("💡 คุณเริ่มมีน้ำหนักเกินเกณฑ์ (ท้วม)")
-else:
-    st.error("🚨 คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย")
+    if bmi < 18.5:
+        st.warning("⚠️ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
+    elif 18.5 <= bmi < 23.0:
+        st.success("🎉 คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
+    elif 23.0 <= bmi < 25.0:
+        st.info("💡 คุณเริ่มมีน้ำหนักเกินเกณฑ์ (ท้วม)")
+    else:
+        st.error("🚨 คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย")
 
 st.divider()
 st.write("ณฐพัฒน์ ไชยพานิชย์ เลขที่ 34 ม.4/3")
